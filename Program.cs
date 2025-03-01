087f2ee70e7f90a2209769c2cd93ea7cf283c85f
Christer <Christer.Fagerstrand@edu.newton.se>
2025-03-01 13:36:54 +0100

    Error during testing connection

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
